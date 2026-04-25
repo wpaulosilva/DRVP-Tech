0d9bde0e077b92b8a116a5734246e53904397e29
Davidsvf <davidvarzimfaria@gmail.com>
2026-04-25 13:01:34 +0100

    Correction of dates in past in requisition and block requisitions of community items

--- DanceSchoolApp.Server/Controllers/Inventory/ItemController.cs
+++ DanceSchoolApp.Server/Controllers/Inventory/ItemController.cs
@@ -75,7 +75,6 @@ namespace DanceSchoolApp.Server.Controllers.Inventory
         {
             try
             {
-                //var result = await _itemService.GetItemsAsync(fromSchool, ownerId: null, query ?? new PagedQuery());
                 var result = await _itemService.GetItemsAsync(fromSchool, query ?? new PagedQuery());
 
                 if (result.TotalCount == 0)
@@ -117,7 +116,6 @@ namespace DanceSchoolApp.Server.Controllers.Inventory
         {
             try
             {
-                //var result = await _itemService.GetItemsAsync(fromSchool: true, ownerId: null, query ?? new PagedQuery());
                 var result = await _itemService.GetItemsAsync(true, query ?? new PagedQuery());
 
                 if (result.TotalCount == 0)
@@ -139,7 +137,6 @@ namespace DanceSchoolApp.Server.Controllers.Inventory
         {
             try
             {
-                //var result = await _itemService.GetItemsAsync(fromSchool: false, ownerId: null, query ?? new PagedQuery());
                 var result = await _itemService.GetItemsAsync(false, query ?? new PagedQuery());
 
                 if (result.TotalCount == 0)
